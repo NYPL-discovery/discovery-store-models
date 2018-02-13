f7210275a313b54064c1081aa0df9b218fcada30
Paul Beaudoin <contact@paulbeaudoin.com>
2018-02-13 17:02:09 -0500

    Add setLogLevel hook, default "info"

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const Item = require('./lib/models/Item')
 const Bib = require('./lib/models/Bib')
 const db = require('./lib/db')
+const logger = require('./lib/logger')
 
 module.exports = {
   Item,
   Bib,
   connect: db.setConnectionUri,
   connected: db.connected,
+  setLogLevel: (level) => {
+    logger.transports.console.level = level
+  },
   _db: db
 }
