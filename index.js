08f7cfa8f4b442a2f0fbea80b10f41af3988aec7
Paul Beaudoin <contact@paulbeaudoin.com>
2021-05-21 21:25:32 -0400

    Add NyplSourceMapper for deconstructing ids
    Adds NyplSourceMapper util for deconstructing discovery store ids (e.g.
    b12082323) into nyplSource, id, and type (bib/item). This allows us to
    centralize our partner institution identifiers, including the newly
    added Harvard (recap-hl)
    
    https://jira.nypl.org/browse/SCC-2719

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Item = require('./lib/models/item')
 const Bib = require('./lib/models/bib')
+const NyplSourceMapper = require('./lib/nypl-source-mapper')
 const db = require('./lib/db')
 const logger = require('./lib/logger')
 
 module.exports = {
   Item,
   Bib,
+  NyplSourceMapper,
   connect: db.setConnectionUri,
   connected: db.connected,
   setLogLevel: (level) => {
