5d209f147ed2e4ca8dde1788072ce5ce7af74611
Paul Beaudoin <contact@paulbeaudoin.com>
2021-06-03 20:42:19 -0400

    Remove temporary NYPL_CORE_VERSION in tests

--- test/helper.js
+++ test/helper.js
@@ -1,13 +1,6 @@
 global.expect = require('chai').expect
 
 const originalCoreVersion = process.env.NYPL_CORE_VERSION
-before(() => {
-  // TODO: Setting a hard NYPL-Core version is temporary (although largely
-  // future safe) and may be removed once the following is merged to
-  // `master`:
-  // https://github.com/NYPL/nypl-core/commit/e7548eaedd93c7dcbe17c82f61e299dfca1a9e13
-  process.env.NYPL_CORE_VERSION = 'v1.37a'
-})
 
 after(() => {
   process.env.NYPL_CORE_VERSION = originalCoreVersion
